00bf5ab600e676c6fa1b20df503299122900d468
Mougily <92stephaniejones@gmail.com>
2023-02-07 11:03:06 +0000

    stuff

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import Articles from "./components/AllArticles";
 import SingleArticle from "./components/SingleArticle";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Home />} />
         <Route path="/articles" element={<Articles />} />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
       </Routes>
---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h2>this is the home page</h2>;
+};
+
+export default Home;
