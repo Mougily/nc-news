889027570ebebe37b1dd17e3eb9b5d85d69f9ccb
Mougily <92stephaniejones@gmail.com>
2023-02-07 10:41:55 +0000

    link to home page in universal header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import Articles from "./components/AllArticles";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Home />} />
         <Route path="/articles" element={<Articles />} />
       </Routes>
     </div>
