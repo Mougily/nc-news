8ab17acfd804362c08c7b79ab5f9d26cac4a7b87
Mougily <92stephaniejones@gmail.com>
2023-02-07 12:31:48 +0000

    began working on viewing comments for single article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
-import { getArticleById } from "../utils/ApiCalls";
+import { getArticleById, getArticleCommentsById } from "../utils/ApiCalls";
 import { useParams } from "react-router-dom";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
   const [loading, setLoading] = useState(false);
+  const [comments, setComments] = useState([]);
 
   const { article_id } = useParams();
 
   useEffect(() => {
     setLoading(true);
-    getArticleById(article_id).then((article) => {
+    Promise.all([
+      getArticleById(article_id),
+      getArticleCommentsById(article_id),
+    ]).then(([article, comments]) => {
       setArticle(article);
+      setComments(comments);
       setLoading(false);
     });
   }, [article_id]);
@@ -19,6 +24,22 @@ const SingleArticle = () => {
   if (loading) {
     return <h2>Still loading...</h2>;
   }
+
+  const commentsHandler = () => {
+    const commentsList = comments.data.comments;
+    return (
+      <p>
+        <h1>hello</h1>
+        {commentsList.map((comment) => {
+          <div>
+            <ol>{comment.author}</ol>
+            <ol>{comment.body}</ol>
+            <ol>{comment.votes}</ol>
+          </div>;
+        })}
+      </p>
+    );
+  };
   return (
     <div className="single_article">
       <h2 className="single_article_title">{article.title}</h2>
@@ -31,6 +52,8 @@ const SingleArticle = () => {
       <p className="author">author: {article.author}</p>
       <p className="data_published">date published: {article.created_at}</p>
       <p className="article_body">{article.body}</p>
+      <button onClick={commentsHandler}>view article comments</button>
+      {comments.length > 0 && commentsHandler()}
     </div>
   );
 };
