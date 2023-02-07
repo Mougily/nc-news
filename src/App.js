6b7a2a1ddab9fe9c30cbf98ffd2a9c9e57fc9e03
Mougily <92stephaniejones@gmail.com>
2023-02-07 11:52:33 +0000

    adding new route to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+// import "./App.css";
+// import Header from "./components/Header";
+// import { Routes, Route } from "react-router-dom";
+// import Articles from "./components/AllArticles";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Articles />} />
+//         <Route path="/articles" element={<Articles />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import Articles from "./components/AllArticles";
+import SingleArticle from "./components/SingleArticle";
 
 function App() {
   return (
@@ -10,6 +30,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Articles />} />
         <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
       </Routes>
     </div>
   );
